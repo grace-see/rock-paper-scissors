40f556fc401ed2aa5942228c6d7cd6e51b0909b2
Grace <171896811+gracedespairs@users.noreply.github.com>
2025-07-18 16:01:39 -0500

    Write logic for playing a round

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -20,6 +20,52 @@ function getHumanChoice() {
     }
 }
 
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "rock") {
+        if (computerChoice === "rock") {
+            console.log("Tie! Rock doesn't beat Rock.");
+        }
+        else if (computerChoice === "paper") {
+            console.log("You lose! Paper beats Rock.");
+            computerScore++;
+        }
+        else if (computerChoice === "scissors") {
+            console.log("You win! Rock beats Scissors.");
+            humanScore++;
+        }
+    }
+    else if (humanChoice === "paper") {
+        if (computerChoice === "rock") {
+            console.log("You win! Paper beats Rock.");
+            humanScore++;
+        }
+        else if (computerChoice === "paper") {
+            console.log("Tie! Paper doesn't beat Paper.");
+        }
+        else if (computerChoice === "scissors") {
+            console.log("You lose! Scissors beats Paper.");
+            computerScore++;
+        }
+    }
+    else if (humanChoice === "scissors") {
+        if (computerChoice === "rock") {
+            console.log("You lose! Scissors beats Rock.");
+            computerScore++;
+        }
+        else if (computerChoice === "paper") {
+            console.log("You win! Scissors beats Paper.");
+            humanScore++;
+        }
+        else if (computerChoice === "scissors") {
+            console.log("Tie! Scissors doesn't beat Scissors.");
+        }
+    }
+}
+
+//Scores for the game
+let humanScore = 0;
+let computerScore = 0;
+
 let playerChoice = getHumanChoice();
 console.log("my choice: "+playerChoice);
 console.log(getComputerChoice());
