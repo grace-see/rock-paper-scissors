65e49b3f62ea5e5f1a796477e518f42360b9e5cf
Grace <171896811+gracedespairs@users.noreply.github.com>
2025-07-31 23:19:42 -0500

    Added win condition and changed some of the logic.

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -8,80 +8,59 @@ function getComputerChoice() {
         return "scissors";
 }
 
-function getHumanChoice() {
-    let choice = window.prompt("Enter a choice: rock, paper, or scissors");
-    switch (choice.toLowerCase()) {
-        case "rock":
-            return "rock";
-        case "paper":
-            return "paper";
-        case "scissors":
-            return "scissors";
-    }
-}
-
 function playRound(humanChoice, computerChoice) {
+    if (hasWon) //One player has hit 5 points, so stop playing the game
+        return;
     if (humanChoice === computerChoice) {
         roundMessage.textContent = "Tie!";
-        //console.log("Tie!");
-        return;
     }
     if (humanChoice === "rock") {
         if (computerChoice === "paper") {
             roundMessage.textContent = "You lose! Paper beats Rock.";
-            //console.log("You lose! Paper beats Rock.");
-            return "c";
+            computerScoreBoard.textContent = "Computer's Score: " + ++computerScore;
         }
         else if (computerChoice === "scissors") {
             roundMessage.textContent = "You win! Rock beats Scissors.";
-            //console.log("You win! Rock beats Scissors.");
-            return "h";
+            humanScoreBoard.textContent = "Player's Score: " + ++humanScore;
         }
     }
     else if (humanChoice === "paper") {
         if (computerChoice === "rock") {
             roundMessage.textContent = "You win! Paper beats Rock.";
-            //console.log("You win! Paper beats Rock.");
-            return "h";
+            humanScoreBoard.textContent = "Player's Score: " + ++humanScore;
         }
         else if (computerChoice === "scissors") {
             roundMessage.textContent = "You lose! Scissors beats Paper.";
-            //console.log("You lose! Scissors beats Paper.");
-            return "c";
+            computerScoreBoard.textContent = "Computer's Score: " + ++computerScore;
         }
     }
     else if (humanChoice === "scissors") {
         if (computerChoice === "rock") {
             roundMessage.textContent = "You lose! Scissors beats Rock.";
-            //console.log("You lose! Scissors beats Rock.");
-            return "c";
+            computerScoreBoard.textContent = "Computer's Score: " + ++computerScore;
         }
         else if (computerChoice === "paper") {
             roundMessage.textContent = "You win! Scissors beats Paper.";
-            //console.log("You win! Scissors beats Paper.");
-            return "h";
+            humanScoreBoard.textContent = "Player's Score: " + ++humanScore;
         }
+    }
+    checkWin();
+}
+
+function checkWin() {
+    if (humanScore >= 5) {
+        roundMessage.textContent = "You win!";
+        hasWon = true;
+    }
+    else if (computerScore >= 5) {
+        roundMessage.textContent = "You lose.";
+        hasWon = true;
     }
 }
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-    let humanChoice, computerChoice;
-    for (let i = 0; i < 5; i++) {
-        humanChoice = getHumanChoice();
-        computerChoice = getComputerChoice();
-        let winner = playRound(humanChoice, computerChoice);
-        switch (winner) {
-            case "c":
-                computerScore++;
-                break;
-            case "h":
-                humanScore++;
-                break;
-        }
-    }
-}
+let humanScore = 0;
+let computerScore = 0;
+let hasWon = false;
 
 //add event listeners to the buttons
 const rockBtn = document.querySelector("#rock");
@@ -99,9 +78,13 @@ scissorsBtn.addEventListener("click", () => {
     playRound("scissors", getComputerChoice());
 });
 
+//add in round results and current score of both players
 const resultsDiv = document.querySelector("#results");
 const roundMessage = document.createElement("p");
 resultsDiv.appendChild(roundMessage);
-
-//play the game
-playGame();
+const humanScoreBoard = document.createElement("p");
+humanScoreBoard.textContent = "Player's Score: 0";
+const computerScoreBoard = document.createElement("p");
+computerScoreBoard.textContent = "Computer's Score: 0";
+resultsDiv.appendChild(humanScoreBoard);
+resultsDiv.appendChild(computerScoreBoard);
